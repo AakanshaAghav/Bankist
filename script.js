a32ebb38b2ee1020023670c4b563f1be43f9c225
AakanshaAghav <aakanshaaghav4@gmail.com>
2025-08-21 19:05:28 +0530

    Implement displayMovements function to render deposits and withdrawals

--- script.js
+++ script.js
@@ -61,6 +61,24 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+      <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+          <div class="movements__value">${mov}€</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -118,23 +136,24 @@ console.log(arr.at(-1)); // this is adv. of 'at' method
 console.log('Sakshi'.at(0)); // also work with strings
 
 */
+/*
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 for (const [i, movement] of movements.entries()) {
   if (movement > 0) {
-    console.log(`Movement ${i+1}: You deposited ${movement}`);
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
   } else {
-    console.log(`Movement ${i+1}: You withdrew ${Math.abs(movement)}`);
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
   }
 };
 
 console.log('---ForEach---');
 
-movements.forEach(function(mov, i, arr){
+movements.forEach(function (mov, i, arr) {
   if (mov > 0) {
-    console.log(`Movement ${i+1}: You deposited ${mov}`);
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
   } else {
-    console.log(`Movement ${i+1}: You withdrew ${Math.abs(mov)}`);
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
   }
 });
 
@@ -145,7 +164,7 @@ const currencies = new Map([
 ]);
 
 //for each with map
-currencies.forEach(function(value, key, map){
+currencies.forEach(function (value, key, map) {
   console.log(`${key}: ${value}`);
 });
 
@@ -154,12 +173,46 @@ const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
 
 console.log(currenciesUnique);
 
-currenciesUnique.forEach(function(value, _, set){
+currenciesUnique.forEach(function (value, _, set) {
   console.log(`${value}: ${value}`);
 });
+*/
 
+// Coding Challenge #1
 
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
 
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  const juliaDogs = dogsJulia.slice(1, -2);
+
+  const dogs = juliaDogs.concat(dogsKate);
+
+  dogs.forEach(function (age, i) {
+    if (age >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+    }
+  });
+};
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 
 
 
